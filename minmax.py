e3fdd9b36eb6cfa20f14cc8e5c628583c02dccf1
Milos <m.jovanovic@elfak.rs>
2022-05-08 01:26:17 +0200

    potez u tuple

--- minmax.py
+++ minmax.py
@@ -57,15 +57,17 @@ def minmax(
                 return beta
         return alpha
 
-def form_action(type,figure_coords:tuple,id,target:tuple,figure_type):
-    action={
+
+def form_action(type, figure_coords: tuple, id, target: tuple, figure_type):
+    action = {
         "type": type,
-        'figureCoords': {'x': figure_coords[0],'y': figure_coords[1]},
+        'figureCoords': {'x': figure_coords[0], 'y': figure_coords[1]},
         'playerID': id,
-        'targetCoords': {'x': target[0],'y': target[1]},
+        'targetCoords': {'x': target[0], 'y': target[1]},
         'figureType': figure_type
     }
     return action
+
 
 def generate_next_states(state, on_turn_max, on_turn_min, is_player_min):
     on_turn = on_turn_min if is_player_min else on_turn_max
@@ -74,7 +76,8 @@ def generate_next_states(state, on_turn_max, on_turn_min, is_player_min):
     opponent_figures_indexes = []
     new_states = []
 
-    action={'type': 1,'figureCoords': {'x': 1,'y': 2},'playerID': '123','targetCoords': {'x': 1,'y': 2},'figureType': 0}
+    action = {'type': 1, 'figureCoords': {'x': 1, 'y': 2},
+              'playerID': '123', 'targetCoords': {'x': 1, 'y': 2}, 'figureType': 0}
 
     for index, figure in state:
         if figure["playerID"] == on_turn:
@@ -89,7 +92,8 @@ def generate_next_states(state, on_turn_max, on_turn_min, is_player_min):
             posible_positions = generate_figure_moves(
                 state, state[index], INFANTRY, INFANTRY_PLACE_MATRIX)
             for move in posible_positions:
-                new_states.append((deepcopy(state),form_action(0,(state[index]["coordX"],state[index]["coordY"]),on_turn,move,state[index]["figureType"])))
+                new_states.append((deepcopy(state), form_action(
+                    0, (state[index]["coordX"], state[index]["coordY"]), on_turn, move, state[index]["figureType"])))
                 new_states[-1][0][index]["coordX"] = move[0]
                 new_states[-1][0][index]["coordY"] = move[1]
 
@@ -97,7 +101,8 @@ def generate_next_states(state, on_turn_max, on_turn_min, is_player_min):
             posible_positions = generate_figure_moves(
                 state, state[index], GUNNER, GUNNER_PLACE_MATRIX)
             for move in posible_positions:
-                new_states.append((deepcopy(state),form_action(0,(state[index]["coordX"],state[index]["coordY"]),on_turn,move,state[index]["figureType"])))
+                new_states.append((deepcopy(state), form_action(
+                    0, (state[index]["coordX"], state[index]["coordY"]), on_turn, move, state[index]["figureType"])))
                 new_states[-1][0][index]["coordX"] = move[0]
                 new_states[-1][0][index]["coordY"] = move[1]
 
@@ -105,19 +110,21 @@ def generate_next_states(state, on_turn_max, on_turn_min, is_player_min):
             posible_positions = generate_figure_moves(
                 state, state[index], MORTAR, MORTAR_PLACE_MATRIX)
             for move in posible_positions:
-                new_states.append((deepcopy(state),form_action(0,(state[index]["coordX"],state[index]["coordY"]),on_turn,move,state[index]["figureType"])))
+                new_states.append((deepcopy(state), form_action(
+                    0, (state[index]["coordX"], state[index]["coordY"]), on_turn, move, state[index]["figureType"])))
                 new_states[-1][0][index]["coordX"] = move[0]
                 new_states[-1][0][index]["coordY"] = move[1]
         else:
             posible_positions = generate_figure_moves(
                 state, state[index], COMMANDO, COMMANDO_PLACE_MATRIX)
             for move in posible_positions:
-                new_states.append((deepcopy(state),form_action(0,(state[index]["coordX"],state[index]["coordY"]),on_turn,move,state[index]["figureType"])))
+                new_states.append((deepcopy(state), form_action(
+                    0, (state[index]["coordX"], state[index]["coordY"]), on_turn, move, state[index]["figureType"])))
                 new_states[-1][0][index]["coordX"] = move[0]
                 new_states[-1][0][index]["coordY"] = move[1]
 
     # attacks
-    attacked_opponent_indexes = []
+    eating_pair_indexes = []  # tuple(index protivik,my index)
     commando_index = None
     # optimizacija jer napadnute pozicije se mnogo malo razlikuju izmedju stanja
     for opponent_index in opponent_figures_indexes:
@@ -127,17 +134,38 @@ def generate_next_states(state, on_turn_max, on_turn_min, is_player_min):
                 continue
 
             if is_attacked(state[index], state[opponent_index], state):
-                attacked_opponent_indexes.append(opponent_index)
+                eating_pair_indexes.append((opponent_index, index))
                 break
 
-    for oppoenet_to_eat in attacked_opponent_indexes:
-        new_states.append(deepcopy(state))
-        del new_states[-1][oppoenet_to_eat]
+    for eating_pair in eating_pair_indexes:
+        new_states.append(
+            (deepcopy(state),
+             form_action(1,
+             (state[eating_pair[1]]["coordX"],
+              state[eating_pair[1]]["coordY"]),
+              on_turn,
+              (state[eating_pair[0]]["coordX"],
+              state[eating_pair[0]]["coordY"]),
+              state[eating_pair[1]]["figureType"]))
+        )
+
+        # new_states.append(deepcopy(state))
+        del new_states[-1][eating_pair[0]]
 
     if commando_index:
         for opponent_index in opponent_figures_indexes:
             if is_attacked(state[commando_index], state[opponent_index], state):
-                new_states.append(deepcopy(state))
+
+                new_states.append(
+                    (deepcopy(state),
+                    form_action(1,
+                    (state[commando_index]["coordX"],
+                    state[commando_index]["coordY"]),
+                    on_turn,
+                    (state[opponent_index]["coordX"],
+                    state[opponent_index]["coordY"]),
+                    commando_index))
+                    )
 
                 new_states[-1][commando_index]["coordX"] = state[opponent_index]["coordX"]
                 new_states[-1][commando_index]["coordY"] = state[opponent_index]["coordY"]
@@ -164,22 +192,22 @@ def generate_figure_moves(game_state, figure, fig_type, fig_move_matrix):
                             q.put(new_pos)
     return all_moves
 
-def can_commando_attack(game_state,figure,victim):
-    all_moves=set()
-    if(figure["figureType"]==COMMANDO):
-        origin=(figure["coordX"],figure["coordY"])
-        q=Queue()
+
+def can_commando_attack(game_state, figure, victim):
+    all_moves = set()
+    if(figure["figureType"] == COMMANDO):
+        origin = (figure["coordX"], figure["coordY"])
+        q = Queue()
         q.put(origin)
         while q:
-            pos=q.get()
-            for x in range(-1,2):
-                for y in range(-1,2):
-                    new_pos=(pos[0]+x,pos[1]+y)
-                    if x!=new_pos[0] and y!=new_pos[1] and new_pos!=origin and 13>new_pos[0]>-1 and 11>new_pos[1]>-1 and (is_free(game_state,new_pos[0],new_pos[1]) or new_pos==victim) and (new_pos[0]-origin[0],new_pos[1]-origin[1]) in COMMANDO_ATTACK_MATRIX:
+            pos = q.get()
+            for x in range(-1, 2):
+                for y in range(-1, 2):
+                    new_pos = (pos[0]+x, pos[1]+y)
+                    if x != new_pos[0] and y != new_pos[1] and new_pos != origin and 13 > new_pos[0] > -1 and 11 > new_pos[1] > -1 and (is_free(game_state, new_pos[0], new_pos[1]) or new_pos == victim) and (new_pos[0]-origin[0], new_pos[1]-origin[1]) in COMMANDO_ATTACK_MATRIX:
                         if new_pos not in all_moves:
-                            if(new_pos==victim):
+                            if(new_pos == victim):
                                 return True
                             all_moves.add(new_pos)
                             q.put(new_pos)
     return False
-
