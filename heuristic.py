05494dce122185fa5feee73275671219feee3b0b
Milos <m.jovanovic@elfak.rs>
2022-05-07 18:45:38 +0200

    nest

--- heuristic.py
+++ heuristic.py
@@ -1,2 +1,4 @@
 
 
+def heuristic(state):
+    pass
--- minmax.py
+++ minmax.py
@@ -1,2 +1,25 @@
-def minmax(state,depth,my_move):
-    None
+from heuristic import heuristic
+
+
+def minmax(
+    state,
+    depth,
+    is_player_min,
+    alpha,
+    beta,
+    #my_move,
+):
+    if depth == 0:
+        return (state, depth, is_player_min, alpha, beta, heuristic(state))
+
+    for new_state in generate_next_states(state, is_player_min):
+        pass
+
+
+
+
+    return None
+
+
+def generate_next_states(state, is_player_min):
+    pass
