1365f0c41decdb8553a2b5b01bb58fcdbb7cfd51
Milos <m.jovanovic@elfak.rs>
2022-05-07 20:07:19 +0200

    minmax kao

--- minmax.py
+++ minmax.py
@@ -1,60 +1,77 @@
 from functools import cache
 from constants import ALPHA_START, BETA_START
-from heuristic import heuristic
+from heuristic import evaluate
 
 
 @cache
 def minmax(
     state,
     depth,
-    is_player_min,
-    alpha=ALPHA_START,
-    beta=BETA_START,
-    # my_move,
-):
+    on_turn_max,
+    on_turn_min,
+    is_player_min=False,
+    alpha=(ALPHA_START, None),
+    beta=(BETA_START, None),
+    my_move=None,
+):  # vraca (value,new_state)
     if depth == 0:
-        return (
-            # state,
-            # depth,
-            # is_player_min,
-            # alpha,
-            # beta,
-            heuristic(state)
-        )
+        return
+        # state,
+        # depth,
+        # is_player_min,
+        # alpha,
+        # beta,
+        (evaluate(state, is_player_min if on_turn_min else on_turn_max), my_move)
 
     if is_player_min:
-        for new_state in generate_next_states(state, is_player_min):
+        for new_state in generate_next_states(state, on_turn_max, on_turn_min, True):
             beta = min(
                 beta,
                 minmax(
                     new_state,
                     depth-1,
+                    on_turn_max,
+                    on_turn_min,
                     False,
                     alpha,
-                    beta
-
-                )
-            )
-            if alpha >= beta:
+                    beta,
+                    new_state if my_move is None else my_move
+                ),
+                key=lambda x: x[0])
+            if alpha[0] >= beta[0]:
                 return alpha
         return beta
 
     else:  # maxplayer
-        for new_state in generate_next_states(state, is_player_min):
+        for new_state in generate_next_states(state, on_turn_max, on_turn_min, False):
             alpha = max(
                 alpha,
                 minmax(
                     new_state,
                     depth-1,
+                    on_turn_max,
+                    on_turn_min,
                     True,
                     alpha,
-                    beta
-                )
-            )
-            if alpha >= beta:
+                    beta,
+                    new_state if my_move is None else my_move
+                ),
+                key=lambda x: x[0])
+            if alpha[0] >= beta[0]:
                 return beta
         return alpha
 
 
-def generate_next_states(state, is_player_min):
+def generate_next_states(state, on_turn_max, on_turn_min, is_player_min):
+    #generate_all_moves filtrirati validne poteze
     pass
+
+def generate_all_moves(state, on_turn_max, on_turn_min, is_player_min):
+    on_turn=on_turn_min if is_player_min else on_turn_max
+    my_figures=[]
+    new_states=[]
+
+    for figure in state:
+        if figure["playerID"]== on_turn:
+            my_figures.append(figure) 
+    
