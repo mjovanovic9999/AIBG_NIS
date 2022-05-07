17b41c976f713a3d4f4b2eb8eb3a1cc2f5b3c082
Milos <m.jovanovic@elfak.rs>
2022-05-07 19:06:49 +0200

    minmax

--- minmax.py
+++ minmax.py
@@ -1,24 +1,59 @@
+from functools import cache
+from constants import ALPHA_START, BETA_START
 from heuristic import heuristic
 
 
+@cache
 def minmax(
     state,
     depth,
     is_player_min,
-    alpha,
-    beta,
-    #my_move,
+    alpha=ALPHA_START,
+    beta=BETA_START,
+    # my_move,
 ):
     if depth == 0:
-        return (state, depth, is_player_min, alpha, beta, heuristic(state))
+        return (
+            # state,
+            # depth,
+            # is_player_min,
+            # alpha,
+            # beta,
+            heuristic(state)
+        )
 
-    for new_state in generate_next_states(state, is_player_min):
-        pass
+    if is_player_min:
+        for new_state in generate_next_states(state, is_player_min):
+            beta = min(
+                beta,
+                minmax(
+                    new_state,
+                    depth-1,
+                    False,
+                    alpha,
+                    beta
 
+                )
+            )
+            if alpha >= beta:
+                return alpha
+        return beta
 
-
-
-    return None
+    else:  # maxplayer
+        for new_state in generate_next_states(state, is_player_min):
+            alpha = max(
+                alpha,
+                minmax(
+                    new_state,
+                    depth-1,
+                    True,
+                    alpha,
+                    beta
+                )
+            )
+            if alpha >= beta:
+                return beta
+        return alpha
 
 
 def generate_next_states(state, is_player_min):
