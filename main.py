80c3349564d0d6906a20d43669b5ac3273effa1d
Milos <m.jovanovic@elfak.rs>
2022-05-07 18:12:22 +0200

    e

--- main.py
+++ main.py
@@ -4,7 +4,4 @@ game_state=[{ "coordX": 0, "coordY": 0, "figureType": 0, "playerID": "123"},
             { "coordX": 1, "coordY": 2, "figureType": 2, "playerID": "" },
             { "coordX": 1, "coordY": 3, "figureType": 1, "playerID": "123" }]
 
-infantry=0
-gunner=1
-mortar=2
-commando=3
+
